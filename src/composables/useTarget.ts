69619cde93719d2fc07e54cfd310d8e3ff81df32
banknight <tufeigd@gmail.com>
2023-10-20 14:19:44 +0800

    整理代码

--- src/composables/useTarget.ts
+++ src/composables/useTarget.ts
@@ -2,7 +2,7 @@ import { Router, Service, Target } from "../types";
 import { toTypeRouter } from "../utils/toTypeRouter";
 import { useKite } from "./useKite";
 
-export function useRemote(service: Service, router?: Router) {
+export function useTarget(service: Service, router?: Router) {
 
     const kite = useKite()
     const target = new Target()
